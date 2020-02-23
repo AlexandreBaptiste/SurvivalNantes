2909e9e6ab1506c6fc39dbbbf381d2dcadd97df9
ScarletArnett <alex61210@hotmail.fr>
2020-02-23 20:11:17 +0100

    Publishing to GitHub Pages

--- rpg-survival/src/environments/environment.prod.ts
+++ rpg-survival/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoidHN1bmRlcmUiLCJhIjoiY2s2czcwZ3kzMGNkdjNscXZpbTQ1Zmh4ZCJ9.3rTm46cDKJqMGeDGwTi9yg'
+  }
 };
