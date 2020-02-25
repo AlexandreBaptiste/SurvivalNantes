d927efd63a08fae1a657a7f28258cc789078e07e
ScarletArnett <alex61210@hotmail.fr>
2020-02-25 22:02:42 +0100

    UI Improvment

--- rpg-survival/src/app/Profile/profile.component.ts
+++ rpg-survival/src/app/Profile/profile.component.ts
@@ -22,12 +22,25 @@ export class ProfileComponent implements OnInit {
   rightHandSelected : boolean = false;
   leftFootSelected : boolean = false;
   rightFootSelected : boolean = false;
+  headSelectedA : boolean = false;
+  leftShoulderSelectedA : boolean = false;
+  rightShoulderSelectedA : boolean = false;
+  leftArmSelectedA : boolean = false;
+  rightArmSelectedA : boolean = false;
+  chestSelectedA : boolean = false;
+  stomachSelectedA : boolean = false;
+  leftLegSelectedA : boolean = false;
+  rightLegSelectedA : boolean = false;
+  leftHandSelectedA : boolean = false;
+  rightHandSelectedA : boolean = false;
+  leftFootSelectedA : boolean = false;
+  rightFootSelectedA : boolean = false;
 
   // Stress & HP
   hp : number = 100;
-  stress : number = 10;
+  stress : number = 25;
   hpAymeric : number = 100;
-  stressAymeric : number = 0;
+  stressAymeric : number = 25;
 
   // Cards
   mehdiCardOpacity   : number = 0;
@@ -85,6 +98,53 @@ export class ProfileComponent implements OnInit {
     }    
   }
 
+  // Change color of the selected body part
+  selectBodyPartA(id: string): void {
+    switch(id){
+      case('head'):
+        this.headSelectedA = (this.headSelectedA) ? false : true;
+        break;
+      case('left-shoulder'):
+        this.leftShoulderSelectedA = (this.leftShoulderSelectedA) ? false : true;
+        break;
+      case('right-shoulder'):
+        this.rightShoulderSelectedA = (this.rightShoulderSelectedA) ? false : true;
+        break;
+      case('left-arm'):
+        this.leftArmSelectedA = (this.leftArmSelectedA) ? false : true;
+        break;
+      case('right-arm'):
+        this.rightArmSelectedA = (this.rightArmSelectedA) ? false : true;
+        break;
+      case('chest'):
+        this.chestSelectedA = (this.chestSelectedA) ? false : true;
+        break;
+      case('stomach'):
+        this.stomachSelectedA = (this.stomachSelectedA) ? false : true;
+        break;
+      case('right-leg'):
+        this.rightLegSelectedA = (this.rightLegSelectedA) ? false : true;
+        break;
+      case('left-leg'):
+        this.leftLegSelectedA = (this.leftLegSelectedA) ? false : true;
+        break;
+      case('right-hand'):
+        this.rightHandSelectedA = (this.rightHandSelectedA) ? false : true;
+        break;
+      case('left-hand'):
+        this.leftHandSelectedA = (this.leftHandSelectedA) ? false : true;
+        break;
+      case('right-foot'):
+        this.rightFootSelectedA = (this.rightFootSelectedA) ? false : true;
+        break;
+      case('left-foot'):
+        this.leftFootSelectedA = (this.leftFootSelectedA) ? false : true;
+        break;
+      default:
+        break;
+    }    
+  }
+  
   // Update "HP/STRESS" height for Mehdi
   updateHpStress(form: NgForm): void {
     this.hp = form.value.hpInput;
